0d4ec95cf7f13f6bdccae1388ad9af1e68feda66
Elnur Nabivi <elnurnabivi@gmail.com>
2025-02-09 23:39:33 +0100

    m

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -2,13 +2,7 @@ import './index.scss'
 import DeveloperMain from '../../../assets/images/developer-image3.png'
 
 const Logo = () => {
-  return (
-    <img
-      src={DeveloperMain}
-      className="solid-logo"
-      alt="font-end developer"
-    ></img>
-  )
+  return <img src={DeveloperMain} className="solid-logo" alt=""></img>
 }
 
 export default Logo
